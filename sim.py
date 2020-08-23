24939bb873ce6619150ca3974a037d9cf3063d82
DESKTOP-SSBQPRP\guomengyu <464936321@qq.com>
2020-08-23 16:13:46 +0800

    Address analysis

--- sim.py
+++ sim.py
@@ -8,8 +8,21 @@ class CrossBar:
         self.acc_buffer = [np.zeros((1,128))] * (14*14)
         self.wgt_buffer = [np.zeros((128,128))] * (3*3)
         self.rram_buffer = [np.zeros((128,128))] * (3*3)
+        self.buffer = {} ##addr:(size)
         ##
-    def DepPush(self,l1,l2):
+    def RRAMBufferAlloc(self,size,addr):
+        self.buffer[addr] = size
+        return 1
+    def RRAMBufferCopy(self, from_, from_offset, to, to_offset, size, kind_mask):
+        ## 
+        # print(self.buffer[to])
+        # print(size)
+        assert(self.buffer[to] == size)
+        return 1
+    def RRAMBufferFree(self, addr):
+        del self.buffer[addr]
+        return 1
+    def RRAMDepPush(self,l1,l2):
         if self.lock[l1]:
             print("%d is used, please wait for a while"%(l1))
             return 0
@@ -18,7 +31,7 @@ class CrossBar:
             return 0
         self.lock[l1],self.lock[l2] = True, True
         return 1
-    def DepPop(self,l1,l2):
+    def RRAMDepPop(self,l1,l2):
         if not self.lock[l1]:
             print("want to release a lock %d which is not used" %(l1))
             return 0
@@ -35,7 +48,8 @@ class CrossBar:
             # print(wgt_index)
             self.acc_buffer[out_index] += np.matmul(self.inp_buffer[inp_index],self.rram_buffer[wgt_index])
         return 1
-    def Load2D(self,src,src_elem_offset,x_size,y_size,x_stride,x_pad_before,y_pad_before, x_pad_after, y_pad_after, dst_sram_index, dst_memory_type):
+    def RRAMLoadBuffer2D(self,src,src_addr,src_elem_offset,x_size,y_size,x_stride,x_pad_before,y_pad_before, x_pad_after, y_pad_after, dst_sram_index, dst_memory_type):
+        assert (src_addr in self.buffer.keys())
         if dst_memory_type == 2: ## inp
             self.inp_buffer = [np.zeros((1,128))] * ((x_size + x_pad_after + x_pad_before) * (y_size + y_pad_after + y_pad_before) )
             for i in range(y_size):
@@ -55,8 +69,9 @@ class CrossBar:
         else:
             print("invalid load")
             return 0
-    def Store2D(self,dst,src_sram_index,src_memory_type,dst_elem_offset,x_size,y_size,x_stride):
+    def RRAMStoreBuffer2D(self,dst,dst_addr,src_sram_index,src_memory_type,dst_elem_offset,x_size,y_size,x_stride):
         assert(src_memory_type == 4)
+        assert(dst_addr in self.buffer.keys())
         for i in range(y_size):
             for j in range(x_size):
                 dst[dst_elem_offset + i*x_stride + j] = self.acc_buffer[src_sram_index + i*x_stride + j]
@@ -75,7 +90,7 @@ class CrossBar:
         return 1
     
 if __name__ == '__main__':
-    f = open('code.txt','r')
+    f = open('./code/202008231455.txt','r')
     lines = f.readlines()
     inp = np.random.randint(-128,127,(1,256,14,14))
     wgt = np.random.randint(-128,127,(256,256,3,3))
@@ -87,27 +102,37 @@ if __name__ == '__main__':
     out = np.zeros((2*14*14,128))
     device = CrossBar()
     for line in lines:
-        if "DepPush" in line:
+        if "RRAMBufferAlloc" in line:
+            tmp = line.split()
+            device.RRAMBufferAlloc(int(tmp[1]),tmp[2])
+        elif "RRAMBufferCopy" in line:
+            tmp = line.split()
+            device.RRAMBufferCopy(tmp[1],int(tmp[2]),tmp[3],int(tmp[4]),int(tmp[5]),int(tmp[6]))
+        elif "RRAMBufferFree" in line:
+            tmp = line.split()
+            device.RRAMBufferFree(tmp[1])
+        elif "DepPush" in line:
             lock = line[line.index('(')+1:line.index(')')].replace(',',' ').split()
             assert(len(lock) == 2)
             l1, l2 = lock
-            assert(device.DepPush(int(l1)-1,int(l2)-1))
+            assert(device.RRAMDepPush(int(l1)-1,int(l2)-1))
         elif "DepPop" in line:
             lock = line[line.index('(')+1:line.index(')')].replace(',',' ').split()
             assert(len(lock) == 2)
             l1, l2 = lock
-            assert(device.DepPop(int(l1)-1,int(l2)-1))
+            assert(device.RRAMDepPop(int(l1)-1,int(l2)-1))
         elif "rram_MAC" in line:
             tmp = line[9:].split()
             out_index,inp_index,wgt_index,rst_n = [int(x) for x in tmp]
             assert(device.rram_MAC(out_index,inp_index,wgt_index,rst_n))
         elif "LoadBuffer2D" in line:
-            tmp = line[16:].split()
+            addr = line[line.index('[') + 1:line.index(']')]
+            tmp = line[line.index(' ')+1:][16:].split()
             src_elem_offset,x_size,y_size,x_stride,x_pad_before,y_pad_before, x_pad_after, y_pad_after, dst_sram_index, dst_memory_type = [int(x) for x in tmp]
             if dst_memory_type == 2:
-                assert(device.Load2D(inp,src_elem_offset,x_size,y_size,x_stride,x_pad_before,y_pad_before, x_pad_after, y_pad_after, dst_sram_index, dst_memory_type))
+                assert(device.RRAMLoadBuffer2D(inp,addr,src_elem_offset,x_size,y_size,x_stride,x_pad_before,y_pad_before, x_pad_after, y_pad_after, dst_sram_index, dst_memory_type))
             elif dst_memory_type == 1:
-                assert(device.Load2D(wgt,src_elem_offset,x_size,y_size,x_stride,x_pad_before,y_pad_before, x_pad_after, y_pad_after, dst_sram_index, dst_memory_type))
+                assert(device.RRAMLoadBuffer2D(wgt,addr,src_elem_offset,x_size,y_size,x_stride,x_pad_before,y_pad_before, x_pad_after, y_pad_after, dst_sram_index, dst_memory_type))
             else:
                 assert(0)
         elif "Set" in line:
@@ -119,14 +144,15 @@ if __name__ == '__main__':
             weight_index,rram_index,rst_n = [int(x) for x in tmp]
             assert(device.LWGT(weight_index,rram_index,rst_n))
         elif "StoreBuffer2D" in line:
-            tmp = line[17:].split()
+            addr = line[line.index('[')+1:line.index(']')]
+            tmp = line[line.index(']')+1:][17:].split()
             src_sram_index,src_memory_type,dst_elem_offset,x_size,y_size,x_stride = [int(x) for x in tmp]
-            assert(device.Store2D(out,src_sram_index,src_memory_type,dst_elem_offset,x_size,y_size,x_stride))
-        elif "VTAUopLoopBegin" in line or "VTAUopLoopEnd" in line:
+            assert(device.RRAMStoreBuffer2D(out,addr,src_sram_index,src_memory_type,dst_elem_offset,x_size,y_size,x_stride))
+        elif "RRAMUopLoopBegin" in line or "RRAMUopLoopEnd" in line:
             pass
-        elif "VTAUopPush" in line or "VTAPushALUOp" in line:
+        elif "RRAMUopPush" in line or "RRAMPushALUOp" in line:
             pass
-        elif "VTASynchronize" in line:
+        elif "RRAMSynchronize" in line:
             print("start sim")
         else:
             print(line)
@@ -134,6 +160,7 @@ if __name__ == '__main__':
             exit()
     import tvm
     tvm.testing.assert_allclose(target, out)
-    # print(out[12]) 
+    print('everthing is ok')
+    print(out[12]) 
 
     
